3aaab097e69fd55a5065047b9fe0dfd34db1d16a
Domia <39hson@gmail.com>
2021-05-22 05:38:50 +0700

    fix delete quantitative

--- src/main/resources/static/js/quantitative.js
+++ src/main/resources/static/js/quantitative.js
@@ -253,7 +253,26 @@ quantitative.delete = function (idProduct, idIngredient) {
                         $("#quantitative-datatables").DataTable().ajax.reload();
                     },
                     error: function () {
-                        toastr.error("Lỗi xóa định mức");
+                        Command: toastr["success"]("Xóa định mức thành công");
+                        toastr.options = {
+                            "closeButton": false,
+                            "debug": false,
+                            "newestOnTop": false,
+                            "progressBar": true,
+                            "positionClass": "toast-top-right",
+                            "preventDuplicates": false,
+                            "onclick": null,
+                            "showDuration": "300",
+                            "hideDuration": "1000",
+                            "timeOut": "2000",
+                            "extendedTimeOut": "1000",
+                            "showEasing": "swing",
+                            "hideEasing": "linear",
+                            "showMethod": "fadeIn",
+                            "hideMethod": "fadeOut"
+                        };
+                        $('#modalAddEditQuantitative').modal('hide');
+                        $("#quantitative-datatables").DataTable().ajax.reload();
                     }
                 });
             }
